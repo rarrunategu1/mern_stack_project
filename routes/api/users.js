2660c9e03754c5afd256947fc45e70cce5f51fc4
Rose Parker <parker.rose@ymail.com>
2018-11-08 21:19:25 +0000

    required validate register input

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys')
 const passport = require('passport');
 
+//Load Input Validaton
+const validateRegisterInput = require('../../validation/register');
+
 //Load User model
 const User = require('../../models/User');
 
