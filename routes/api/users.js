12234c87ba5f273e873e3540209370d666e91573
Rose Parker <parker.rose@ymail.com>
2018-11-12 03:05:18 +0000

    required validation for login and updated post route to /login so that it passes in errors instead

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,9 +6,10 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys')
 const passport = require('passport');
 
+
 //Load Input Validaton
 const validateRegisterInput = require('../../validation/register');
-
+const validateLoginInput = require('../../validation/login');
 //Load User model
 const User = require('../../models/User');
 
@@ -67,6 +68,12 @@ router.post('/register', (req, res) => {
 //@access   Public
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  
+  //check validation
+    if(!isValid) {
+      return res.status(400).json(errors)
+    };
     const email = req.body.email;
     const password = req.body.password;
     
@@ -74,7 +81,8 @@ router.post('/login', (req, res) => {
     User.findOne({email})
         .then(user => {
             if (!user) {
-                return res.status(404).json({email: 'User not found'});
+              errors.email = 'User not found'
+                return res.status(404).json(errors);
             }
             
             //Check Password
@@ -97,7 +105,8 @@ router.post('/login', (req, res) => {
                       });
                     });
                 } else {
-                  return res.status(400).json({password: 'Password incorrect'})
+                  errors.password = 'Password incorrect'
+                  return res.status(400).json(errors)
                 }
               })
         })
