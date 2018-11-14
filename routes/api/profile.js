a934b88a814ad8f78e525c084ab2e8ffa48aa691
Rose Parker <parker.rose@ymail.com>
2018-11-14 03:44:18 +0000

    delete route to remove user and profile after authenticated login

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -282,6 +282,21 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false
     })
             .catch(err => res.status(404).json(err));
 }
-)
+);
+
+
+//@route    DELETE api/profile
+//@desc     Delete user and profile
+//@access   Private
+
+router.delete('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+        .then(() => {
+            User.findOneAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: true}))
+        })
+}
+);
+
 
 module.exports = router;
