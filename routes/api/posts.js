38ed3997ef69d9b7d2ab4c6822832c3ef33e9f3e
Rose Parker <parker.rose@ymail.com>
2018-11-15 01:33:50 +0000

    required validation for post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport'); //will be used fo protected routes
 
+//Post Model
 const Post = require('../../models/Post');
+
+//Validation
+const validatePostInput = require('../../validation/post');
+
 
 //@route    GET api/posts/test
 //@desc     Tests post route
@@ -14,10 +19,18 @@ router.get('/test', (req, res) => res.json({msg: 'Posts Works'}));
 //@desc     Create post
 //@access   Private -we don't want just anyone to write a post
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    
+    //check validation
+    if(!isValid) {
+      //If any erros send 400 with erros object
+      return res.status(400).json(errors);
+    }
+    
     const newPost = new Post ({
         text: req.body.text,
         name: req.body.name,
-        avatar: req.body.name,
+        avatar: req.body.avatar,
         user: req.user.id
     });
     
