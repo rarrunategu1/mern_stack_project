1b83c47c41a8c42cb6d2ca2d1f34d4555ae09649
Rose Parker <parker.rose@ymail.com>
2018-11-15 01:09:07 +0000

    added post route for new posts by a user after authentication and saves

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,11 +3,31 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport'); //will be used fo protected routes
 
+const Post = require('../../models/Post');
 
 //@route    GET api/posts/test
 //@desc     Tests post route
 //@access   Public
 router.get('/test', (req, res) => res.json({msg: 'Posts Works'}));
 
-
+//@route    POST api/posts
+//@desc     Create post
+//@access   Private -we don't want just anyone to write a post
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const newPost = new Post ({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.user.id
+    });
+    
+    newPost
+      .save()
+      .then(post => res.json(post));
+})
 module.exports = router;
+
+
+
+
+
