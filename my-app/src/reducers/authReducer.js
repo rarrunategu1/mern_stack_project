625056768b45b7aa570031dd7786150bf30bd378
Rose Parker <parker.rose@ymail.com>
2018-12-28 19:06:23 +0000

    working with type to set current user.  if empty is does not authenticate and displays empty object

--- my-app/src/reducers/authReducer.js
+++ my-app/src/reducers/authReducer.js
@@ -1,4 +1,5 @@
-
+import isEmpty from '../validation/is-empty';
+import { SET_CURRENT_USER } from '../actions/types';
 
 const initialState = {
     isAuthenticated: false, 
@@ -7,7 +8,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
-        
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !isEmpty(action.payload), //will only authenticate if it's a user
+                user: action.payload
+            };
         default:
             return state;
     }
