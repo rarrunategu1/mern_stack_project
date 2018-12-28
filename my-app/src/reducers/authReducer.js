f52601aeedf98200e970196a00f6960891f699ad
Rose Parker <parker.rose@ymail.com>
2018-12-28 02:02:17 +0000

    imported test dispatch to use in the function's switch statement and return the information added to the state

--- my-app/src/reducers/authReducer.js
+++ my-app/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { TEST_DISPATCH } from '../actions/types';
+
 const initialState = {
     isAuthenticated: false, 
     user: {}
@@ -5,6 +7,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case TEST_DISPATCH:
+            return {
+                ...state, //will add to the state
+                user: action.payload //will fill the user with the payload which is the userdata
+            }
         default:
             return state;
     }
