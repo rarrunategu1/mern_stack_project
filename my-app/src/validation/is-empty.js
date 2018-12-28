16c60427f8a2718349a18a1ccf3dae687450c7e3
Rose Parker <parker.rose@ymail.com>
2018-12-28 19:04:58 +0000

    validation to check for is empty

---
+++ my-app/src/validation/is-empty.js
@@ -0,0 +1,8 @@
+//global function to use anywhere
+const isEmpty = value => 
+        value === undefined ||
+        value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0) || //empty object would be 0
+        (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;    
