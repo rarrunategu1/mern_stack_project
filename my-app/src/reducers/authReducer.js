39beb0590d416578c5dc9c3f06b57bd61485ea16
Rose Parker <parker.rose@ymail.com>
2018-12-28 15:36:04 +0000

    removed mention of TEST_DISPATCH to work with GET_ERRORS type that I will create momentarily

--- my-app/src/reducers/authReducer.js
+++ my-app/src/reducers/authReducer.js
@@ -1,17 +1,13 @@
-import { TEST_DISPATCH } from '../actions/types';
+
 
 const initialState = {
     isAuthenticated: false, 
     user: {}
-}
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
-        case TEST_DISPATCH:
-            return {
-                ...state, //will add to the state
-                user: action.payload //will fill the user with the payload which is the userdata
-            }
+        
         default:
             return state;
     }
