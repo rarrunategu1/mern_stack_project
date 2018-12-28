c753425ccbfc41a0e96ec1e0b753a8efcb2a3d25
Rose Parker <parker.rose@ymail.com>
2018-12-28 01:31:37 +0000

    imported rootReducer -index.js file and created an initialState variable to use in the store variable.

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';  //root reducer is index.js file in reducers
 
+const middleware = [thunk];
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore((rootReducer) => [], {}, applyMiddleware(...middleware)); //first parameter is root reducer
 
 export default store;
