2b0fb1149bbe61db3db82ddfc0cf0823576be51c
Rose Parker <parker.rose@ymail.com>
2018-12-28 00:43:18 +0000

    installed classnames to be able to do turn a section red if it's invalid.  Display for errors in each section if invalid

--- my-app/src/components/auth/Register.js
+++ my-app/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
     constructor() { //to create component state
@@ -36,6 +37,8 @@ class Register extends Component {
     }
     
     render() {
+      //if there are errors, they'll be here
+      const { errors } = this.state; //this destructuring is the same as const errors = this.state.errors;
         return(
             <div className="register">
     <div className="container">
@@ -43,44 +46,59 @@ class Register extends Component {
         <div className="col-md-8 m-auto">
           <h1 className="display-4 text-center">Sign Up</h1>
           <p className="lead text-center">Create your DevConnector account</p>
-          <form onSubmit={this.onSubmit}>
+          <form noValidate onSubmit={this.onSubmit}>
             <div className="form-group">
               <input 
               type="text" 
-              className="form-control form-control-lg" 
+              className={classnames('form-control form-control-lg', { //form controls here are classnames that are always going to be used
+                'is-invalid': errors.name //this will only happen if there is an error with the name and it will make the outline red
+              })} 
               placeholder="Name" 
               name="name" 
               value={this.state.name}
               onChange={this.onChange}
               required />
+              {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}  
             </div>
             <div className="form-group">
               <input 
               type="email" 
-              className="form-control form-control-lg" 
+              className={classnames('form-control form-control-lg', { //form controls here are classnames that are always going to be used
+                'is-invalid': errors.email //this will only happen if there is an error with the name and it will make the outline red
+              })} 
               placeholder="Email Address" 
               name="email"
               value={this.state.email}
               onChange={this.onChange}/>
+              {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}  
+            
               <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
             </div>
             <div className="form-group">
               <input 
               type="password" 
-              className="form-control form-control-lg" 
+              className={classnames('form-control form-control-lg', { //form controls here are classnames that are always going to be used
+                'is-invalid': errors.password //this will only happen if there is an error with the name and it will make the outline red
+              })}  
               placeholder="Password" 
               name="password"
               value={this.state.password}
               onChange={this.onChange}/>
+              {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}  
+            
             </div>
             <div className="form-group">
               <input 
               type="password" 
-              className="form-control form-control-lg" 
+              className={classnames('form-control form-control-lg', { //form controls here are classnames that are always going to be used
+                'is-invalid': errors.password2 //this will only happen if there is an error with the name and it will make the outline red
+              })}  
               placeholder="Confirm Password" 
               name="password2"
               value={this.state.password2}
               onChange={this.onChange}/>
+              {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}  
+            
             </div>
             <input type="submit" className="btn btn-info btn-block mt-4" />
           </form>
