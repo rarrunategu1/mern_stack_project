57744f9f9bc8d481ea702b510035c6bb75fcacf1
rarrunategu1 <parker.rose@ymail.com>
2019-01-04 21:37:36 -0500

    corrections made

--- my-app/src/actions/authActions.js
+++ my-app/src/actions/authActions.js
@@ -1,51 +1,54 @@
-import axios from 'axios';
-import setAuthToken from '../utils/setAuthToken';
-import jwt_decode from 'jwt-decode';
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS, SET_CURRENT_USER } from './types';
-
-
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
-    //dispatch something to our reducer
-    axios.post('https://careerdevsintermediate-titamandarica.c9users.io:8081/api/users/register', userData)
-          .then(res => history.push('/login'))//redirect if successful
-          .catch(err => 
-            dispatch({
-              type: GET_ERRORS,
-              payload: err.response.data
-          })
-         );
+  //dispatch something to our reducer
+  axios
+    .post(
+      "https://careerdevsintermediate-titamandarica.c9users.io:8081/api/users/register",
+      userData
+    )
+    .then(res => history.push("/login")) //redirect if successful
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
 
 //Login - get user token
-export const loginUser = (userData) => dispatch => {
-  axios.post('/api/user/login', userData)
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
     .then(res => {
       //save to local storage
       const { token } = res.data;
       //set token to local storage
-      localStorage.setItem('jwtToken', token); //only stores strings
+      localStorage.setItem("jwtToken", token); //only stores strings
       //set toke to Auth header
       setAuthToken(token);
       //decode token to get user data
       const decoded = jwt_decode(token);
       //set current user
       dispatch(setCurrentUser(decoded));
-      
     })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      }));
-}
+      })
+    );
+};
 
 //Set logged in user
-export const setCurrentUser = (decoded) => {
+export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
-  }
-}
+  };
+};
--- my-app/src/components/auth/Login.js
+++ my-app/src/components/auth/Login.js
@@ -1,105 +1,114 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import classnames from 'classnames';
-import { loginUser } from '../../actions/authActions';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { loginUser } from "../../actions/authActions";
 
 class Login extends Component {
-        constructor() { //to create component state
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            errors: {}
-        };
-        
-        this.onChange = this.onChange.bind(this); //binds "this" so that it can be used in the change event function
-        this.onSubmit = this.onSubmit.bind(this);
+  constructor() {
+    //to create component state
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this); //binds "this" so that it can be used in the change event function
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
-    
-    componentWillReceiveProps(nextProps) {
-      if (nextProps.auth.isAuthenticated) {
-        this.props.history.push('/dashboard');
-      }
-      
-      //test for errors prop
-      if (nextProps.errors) {
-        this.setState({errors: nextProps.errors});
-      }
+
+    //test for errors prop
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
-    
-    
-    
-     onSubmit(event) {
-        event.preventDefault();
-        
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        };
-        
-        this.props.loginUser(userData); //logs user on submit
-        
-    }
-    
-    onChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    }
-    
-    render() {
-      
-      const { errors } = this.state;
-      
-        return(
-              <div className="login">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 m-auto">
-          <h1 className="display-4 text-center">Log In</h1>
-          <p className="lead text-center">Sign in to your DevConnector account</p>
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <input type="email" 
-              className={classnames('form-control form-control-lg', { //form controls here are classnames that are always going to be used
-                        'is-invalid': errors.email //this will only happen if there is an error with the name and it will make the outline red
-                      })}  
-              placeholder="Email Address" 
-              name="email"
-              value={this.state.email}
-              onChange={this.onChange}
-              />
-              {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}  
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.loginUser(userData); //logs user on submit
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={classnames("form-control form-control-lg", {
+                      //form controls here are classnames that are always going to be used
+                      "is-invalid": errors.email //this will only happen if there is an error with the name and it will make the outline red
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      //form controls here are classnames that are always going to be used
+                      "is-invalid": errors.password //this will only happen if there is an error with the name and it will make the outline red
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
-            <div className="form-group">
-              <input type="password" 
-               className={classnames('form-control form-control-lg', { //form controls here are classnames that are always going to be used
-                        'is-invalid': errors.password //this will only happen if there is an error with the name and it will make the outline red
-                      })}  
-              placeholder="Password" 
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-              />
-              {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}  
-            </div>
-            <input type="submit" 
-            className="btn btn-info btn-block mt-4" />
-          </form>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-            )
-    }
+    );
+  }
 }
 Login.propTypes = {
   loginUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth,
   errors: state.errors
-})
-export default connect(mapStateToProps, { loginUser })(Login);
+});
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
