d200335de80dabc26914a3d48f9264aeebb4c033
rarrunategu1 <parker.rose@ymail.com>
2019-01-11 23:27:49 -0500

    Brought in the text field group and applied the changes based on TextFieldGroup and passed in default Props if not passed in

--- my-app/src/components/common/TextFieldGroup.js
+++ my-app/src/components/common/TextFieldGroup.js
@@ -13,7 +13,38 @@ const TextFieldGroup = ({
   onChange,
   disabled
 }) => {
-  return <div />;
+  return (
+    <div className="form-group">
+      <input
+        type={type}
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": errors.email //this will only happen if there is an error with the name and it will make the outline red
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.string.isRequired,
+  disabled: PropTypes.string
+};
+//if not passed in
+TextFieldGroup.defaultProps = {
+  type: "text"
 };
 
 export default TextFieldGroup;
