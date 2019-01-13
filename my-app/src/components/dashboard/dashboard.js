69ea7db7b4e7617da4948a834aca25342cf94085
rarrunategu1 <parker.rose@ymail.com>
2019-01-13 16:31:20 -0500

    dashboard will show hello if user is authenticated else it will show loading...

--- my-app/src/components/dashboard/dashboard.js
+++ my-app/src/components/dashboard/dashboard.js
@@ -8,7 +8,29 @@ class Dashboard extends Component {
     this.props.getCurrentProfile();
   }
   render() {
-    return <div />;
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <h4>Loading...</h4>;
+    } else {
+      dashboardContent = <h1>Hello</h1>;
+    }
+
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
