ae991bba89341ba1d0ff6d247efecf67593b8618
rarrunategu1 <parker.rose@ymail.com>
2019-01-13 14:44:26 -0500

    brought in the dashboard to app.js and created a route for it.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,6 +13,7 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashbaord/Dashboard";
 
 import "./App.css";
 
@@ -47,6 +48,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
