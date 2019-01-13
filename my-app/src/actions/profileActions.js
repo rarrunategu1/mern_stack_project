fa0777dc0989c94704f8d90affc6583017991ad6
rarrunategu1 <parker.rose@ymail.com>
2019-01-13 15:03:30 -0500

    profileActions now has an action / function to clear the current profile

--- my-app/src/actions/profileActions.js
+++ my-app/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from "./types";
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from "./types";
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -26,5 +26,12 @@ export const getCurrentProfile = () => dispatch => {
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
+  };
+};
+
+//Clear Profile
+export const setProfileLoading = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
   };
 };
