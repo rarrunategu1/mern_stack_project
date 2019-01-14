509fd6444600b7b6efae18c4c4c67b5933e398b9
rarrunategu1 <parker.rose@ymail.com>
2019-01-13 19:15:55 -0500

    Select List Group

--- my-app/src/components/common/SelectListGroup.js
+++ my-app/src/components/common/SelectListGroup.js
@@ -2,15 +2,7 @@ import React from "react";
 import classnames from "classnames";
 import PropTypes from "prop-types";
 
-const SelectListGroup = ({
-  name,
-  placeholder,
-  value,
-  error,
-  info,
-  onChange,
-  options
-}) => {
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
   const selectOptions = options.map(option => (
     <option key={option.label} value={option.value}>
       {option.label}
@@ -18,15 +10,16 @@ const SelectListGroup = ({
   ));
   return (
     <div className="form-group">
-      <textarea
+      <select
         className={classnames("form-control form-control-lg", {
           "is-invalid": error //this will only happen if there is an error with the name and it will make the outline red
         })}
-        placeholder={placeholder}
         name={name}
         value={value}
         onChange={onChange}
-      />
+      >
+        {selectOptions}
+      </select>
       {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
@@ -35,7 +28,6 @@ const SelectListGroup = ({
 
 SelectListGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
