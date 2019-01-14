90e56afd4e845cd54712ae5708799277957b9d19
rarrunategu1 <parker.rose@ymail.com>
2019-01-14 10:44:48 -0500

    Added social media inputs button which toggles the state when clicked from false to true

--- my-app/src/components/create-profile/CreateProfile.js
+++ my-app/src/components/create-profile/CreateProfile.js
@@ -132,6 +132,20 @@ class CreateProfile extends Component {
                 error={errors.bio}
                 info="Tell us a little about yourself"
               />
+
+              <div className="mb-3">
+                <button
+                  onClick={() => {
+                    this.setState(prevState => ({
+                      displaySocialInputs: !prevState.displaySocialInputs
+                    }));
+                  }}
+                  className="btn btn-light"
+                >
+                  Add Social Network Links
+                </button>
+                <span className="text-muted">Optional</span>
+              </div>
             </form>
           </div>
         </div>
