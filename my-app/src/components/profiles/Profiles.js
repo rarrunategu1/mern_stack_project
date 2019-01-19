7dce8f5dca40c39c0a4301b70b5a18747d3aac67
rarrunategu1 <parker.rose@ymail.com>
2019-01-19 12:11:02 -0500

    brought in ProfileItem to Profiles.js and made some div corrections to display the developer Profiles correctly

--- my-app/src/components/profiles/Profiles.js
+++ my-app/src/components/profiles/Profiles.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
+import ProfileItem from "./ProfileItem";
 import { getProfiles } from "../../actions/profileActions";
 
 class Profiles extends Component {
@@ -19,7 +20,9 @@ class Profiles extends Component {
       profileItems = <Spinner />;
     } else {
       if (profiles.length > 0) {
-        profileItems = <h1>PROFILES HERE</h1>;
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
       } else {
         profileItems = <h4>No profiles found...</h4>;
       }
@@ -28,12 +31,13 @@ class Profiles extends Component {
       <div className="profiles">
         <div className="container">
           <div className="row">
-            <div className="col-md-12" />
-            <h1 className="display-4 text-center">Developer Profiles</h1>
-            <p className="lead text-center">
-              Browse and connect with developers
-            </p>
-            {profileItems}
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and connect with developers
+              </p>
+              {profileItems}
+            </div>
           </div>
         </div>
       </div>
