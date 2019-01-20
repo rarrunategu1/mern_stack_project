0107e270936c8414d2b06f47274c23ab7b329a1b
rarrunategu1 <parker.rose@ymail.com>
2019-01-20 18:19:30 -0500

    Brought in all components of profile subcomponents folder including connect, spinner and proptypes

---
+++ my-app/src/components/profile/Profile.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "/ProfileGithub";
+import Spinner from "../common/Spinner";
+
+class Profile extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps)(Profile);
