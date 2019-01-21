7655f54787f2b0f185618733d024d1bd2a251215
rarrunategu1 <parker.rose@ymail.com>
2019-01-20 20:12:34 -0500

    ProfileHeader username, company and location added dynamically

--- my-app/src/components/profile/ProfileHeader.js
+++ my-app/src/components/profile/ProfileHeader.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileHeader extends Component {
+  //destructuring used to take out the profile from the property that's being passed in to use it
   render() {
+    const { profile } = this.props;
+
     return (
-      <div>
-        <h1>TODO: PROFILE HEADER</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-info text-white mb-3">
+            <div className="row">
+              <div className="col-4 col-md-3 m-auto">
+                <img
+                  className="rounded-circle"
+                  src={profile.user.avatar}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="display-4 text-center">{profile.user.name}</h1>
+              <p className="lead text-center">
+                {profile.status}{" "}
+                {isEmpty(profile.company) ? null : (
+                  <span>at {profile.company}</span>
+                )}
+              </p>
+
+              {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
+
+              <p>
+                <a className="text-white p-2" href="#">
+                  <i className="fas fa-globe fa-2x" />
+                </a>
+                <a className="text-white p-2" href="#">
+                  <i className="fab fa-twitter fa-2x" />
+                </a>
+                <a className="text-white p-2" href="#">
+                  <i className="fab fa-facebook fa-2x" />
+                </a>
+                <a className="text-white p-2" href="#">
+                  <i className="fab fa-linkedin fa-2x" />
+                </a>
+                <a className="text-white p-2" href="#">
+                  <i className="fab fa-instagram fa-2x" />
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
