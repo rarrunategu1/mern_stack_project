4fbf9c7fa161e7c8ca1dd6cf2341bfc794958af7
rarrunategu1 <parker.rose@ymail.com>
2019-01-22 17:46:34 -0500

    created variable to grab users first name and use it dynamically in the render

--- my-app/src/components/profile/ProfileAbout.js
+++ my-app/src/components/profile/ProfileAbout.js
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    //Get first name
+    const firstName = profile.user.name.trim().split(" ")[0];
+
     return (
-      <div>
-        <h1>TODO: PROFILE ABOUT</h1>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-body bg-light mb-3">
+            <h3 class="text-center text-info">{firstName}</h3>
+            <p class="lead">
+              {isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}
+            </p>
+            <hr />
+            <h3 class="text-center text-info">Skill Set</h3>
+            <div class="row">
+              <div class="d-flex flex-wrap justify-content-center align-items-center">
+                <div class="p-3">
+                  <i class="fa fa-check" /> HTML
+                </div>
+                <div class="p-3">
+                  <i class="fa fa-check" /> CSS
+                </div>
+                <div class="p-3">
+                  <i class="fa fa-check" /> JavaScript
+                </div>
+                <div class="p-3">
+                  <i class="fa fa-check" /> Python
+                </div>
+                <div class="p-3">
+                  <i class="fa fa-check" /> C#
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
