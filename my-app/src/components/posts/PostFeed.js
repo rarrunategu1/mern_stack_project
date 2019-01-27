44b2cd47b750763ab01d65d395687252e12953e8
rarrunategu1 <parker.rose@ymail.com>
2019-01-27 16:40:58 -0500

    PostFeed component maps throught the posts and returns the post with post id

---
+++ my-app/src/components/posts/PostFeed.js
@@ -0,0 +1,18 @@
+//mapping through posts
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import PostItem from "./PostItem";
+
+class PostFeed extends Component {
+  render() {
+    const { posts } = this.props;
+
+    return posts.map(post => <PostItem key={post._id} post={post} />);
+  }
+}
+
+PostFeed.propTypes = {
+  posts: PropTypes.array.isRequired
+};
+
+export default PostFeed;
