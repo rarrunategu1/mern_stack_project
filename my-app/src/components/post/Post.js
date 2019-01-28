59eb877d93aac6f0ea82f0b3339d8be9cb49afa7
rarrunategu1 <parker.rose@ymail.com>
2019-01-27 21:47:24 -0500

    showActions brought in to only show like, unlike and comments buttons if it's set to true so that the single post will display without those when comment button is clicked

--- my-app/src/components/post/Post.js
+++ my-app/src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import PostItem from "../posts/PostItem";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../actions/postActions";
 
@@ -9,9 +11,30 @@ class Post extends Component {
     this.props.getPost(this.props.match.params.id);
   }
   render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>POST</h1>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
