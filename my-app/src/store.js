4cccab477b788316ba58ae84a4e3d9be11576f86
rarrunategu1 <parker.rose@ymail.com>
2019-02-03 21:32:01 -0500

    debug

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,20 +1,33 @@
-import { createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';  //root reducer is index.js file in reducers
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers"; //root reducer is index.js file in reducers
 
 const initialState = {};
 
 const middleware = [thunk];
 
+// Developer tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
 const store = createStore(
-    rootReducer, 
-    initialState,
-    //applyMiddleware(thunk),
-    compose(
-         applyMiddleware(...middleware),
-        //implement redux dev tools
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-    ); 
+  rootReducer,
+  initialState,
+  composeSetup(applyMiddleware(...middleware))
+
+  // const store = createStore(
+  //     rootReducer,
+  //     initialState,
+  //     //applyMiddleware(thunk),
+  //     compose(
+  //          applyMiddleware(...middleware),
+  //         //implement redux dev tools
+  //         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //     )
+);
 
 export default store;
